ba9a13b64dc3496357efaf446d40c393dbc21c4b
guagrapio <agrapiogustavo@gmail.com>
2024-12-05 09:56:00 -0300

    Adicionando arquivo da aula 2 - Senao

---
+++ Aula 2 - Gu/Aula 2.py
@@ -0,0 +1,11 @@
+print ('Olá, bom dia a todos!')
+       
+nome_usuario = 'Gustavo Agrapio' # tipo -> str = string
+idade_usuario = 21 # tipo -> int = integer
+altura_usuario = 1.78 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print('seja bem vindo', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('você tem', altura_usuario, 'metros.')
+print('você é maior de idade?', maior_idade)
